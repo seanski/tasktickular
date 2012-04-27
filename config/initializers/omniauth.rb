16f93a093bde092a275543a2bfc759380f7e15bf
Sean Hill <seandhi@gmail.com>
2012-04-27 15:58:18 -0500

    Add initializer for github and google omniauth providers.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  #provider :developer unless Rails.env.production?
+  #provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  #provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,repo,gist"
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+end
