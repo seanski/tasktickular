76e24e2614c8bc3de2a35a0813cb23e285b2beba
Sean Hill <seandhi@gmail.com>
2012-04-27 19:06:31 -0500

    load and authorize user resource.  Remove new and create methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < AuthenticatedController
+  load_and_authorize_resource
+  respond_to :json, :html
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,21 +15,9 @@ class UsersController < AuthenticatedController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.json
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
       format.json { render json: @user }
     end
   end
@@ -37,26 +27,10 @@ class UsersController < AuthenticatedController
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PUT /users/1
   # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
@@ -72,7 +46,7 @@ class UsersController < AuthenticatedController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
+
     @user.destroy
 
     respond_to do |format|
