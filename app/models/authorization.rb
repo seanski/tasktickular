8e5e2e60e4b7d36e21870419f3073714754bdef5
Sean Hill <seandhi@gmail.com>
2012-04-27 19:08:22 -0500

    Create methods for finding or creating authorizations.

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -2,4 +2,32 @@ class Authorization < ActiveRecord::Base
   attr_accessible :email, :first_name, :image, :last_name, :location, :name, :nickname, :provider, :secret, :token, :uid, :user_id
 
   belongs_to :user
+  class << self
+    def find_or_create_from_auth_hash(auth_hash, user)
+      auth = Authorization.credentials(auth_hash['provider'], auth_hash['uid']).with_user.first
+      if auth
+        auth.token = auth_hash['credentials']['token']
+        auth.save
+        auth
+      else
+        create_user_and_auth(auth_hash, user)
+      end
+    end
+
+    def create_user_and_auth(auth_hash, user)
+      info = auth_hash['info']
+      name = info['nickname'] || info['name']
+      email = info['email']
+      credentials = auth_hash['credentials']
+      user = User.create(username: name, email: email) if user.nil?
+      auth = user.authorizations.create(email: email,
+                                        first_name: info['first_name'],
+                                        last_name: info['last_name'],
+                                        location: info['location'],
+                                        provider: auth_hash['provider'],
+                                        token: credentials['token'],
+                                        uid: auth_hash['uid']
+                                        )
+    end
+  end
 end
