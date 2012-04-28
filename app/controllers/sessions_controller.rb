db2876dd650b64dc4a871a6311abfb8a7f188a4e
Sean Hill <seandhi@gmail.com>
2012-04-27 19:07:26 -0500

    Create session and dump registration info on rabbit mq.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def create
+    @user = current_user
+    @auth = Authorization.find_or_create_from_auth_hash(auth_hash, @user)
+    self.current_user = @auth.user.id
+    MQ.publish(@auth.registration_dump)
+    redirect_to(self.current_user)
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
