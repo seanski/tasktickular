261e2f8ba6f584471a14665e77ccc32761439109
Sean Hill <seandhi@gmail.com>
2012-04-27 19:02:30 -0500

    Add methods for getting and setting current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) rescue nil
+  end
+
+  def current_user=(user_id)
+    @current_user = nil
+    session[:user_id] = user_id
+  end
 end
